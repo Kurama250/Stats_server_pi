db7db911cb1816f1af19e0bdb19b593bbeb69df5
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-09-20 22:05:15 +0200

    Add files via upload

--- setup_server.sh
+++ setup_server.sh
@@ -6,7 +6,7 @@ apt update && apt upgrade -y
 apt install npm node.js git raspi-config -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
 apt-get install -y nodejs -y
-git clone https://github.com/Kurama250/Stats_server.git
-cd Stats_server/
+git clone https://github.com/Kurama250/Stats_server_pi.git
+cd Stats_server_pi/
 npm install discord.js@13 child_process
 npm install pm2 -g
