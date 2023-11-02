b91d1057b46fc6acdb905b9ba10dcd4b2adbb275
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-11-02 03:33:25 +0100

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -3,7 +3,7 @@
 # Github : https://github.com/Kurama250
 
 apt update && apt upgrade -y
-apt install npm node.js git raspi-config -y
+apt install npm nodejs git raspi-config -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
 apt-get install -y nodejs -y
 git clone https://github.com/Kurama250/Stats_server_pi.git
