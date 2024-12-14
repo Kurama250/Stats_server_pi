d935d5acb00b3f037b1a3581c4d5cf6792eec1ac
Kurama <60360593+Kurama250@users.noreply.github.com>
2024-12-14 21:29:20 +0100

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-# setup_server by Kurama250
-# Github : https://github.com/Kurama250
+# setup_server.sh by Kurama250
+# Github : https://github.com/Kurama250/
 
 apt update && apt upgrade -y
 apt install npm nodejs git raspi-config -y
+
 if ! command -v node &> /dev/null; then
-    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-    apt-get install -y nodejs -y
+    echo "Node.js is not installed. Installing..."
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+    apt-get install -y nodejs
 else
     echo "Node.js is already installed. Skipping installation."
 fi
+
 git clone https://github.com/Kurama250/Stats_server_pi.git
 cd Stats_server_pi/
 npm install discord.js child_process
-npm install pm2 -g
+
+if ! command -v pm2 &> /dev/null; then
+    echo "PM2 is not installed. Installing..."
+    npm install pm2 -g
+else
+    echo "PM2 is already installed. Skipping installation."
+fi
